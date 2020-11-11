36f156c0dfe48b43c96e3b4cd0be8ce30c0df5e8
enochcho <enochcho97@hotmail.com>
2020-11-10 23:50:04 -0500

    random

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import {
   Card,
   Container,
@@ -8,24 +8,14 @@ import {
   Button,
   Image,
 } from "react-bootstrap";
-import { useDispatch, useSelector } from "react-redux";
-import { onLogin } from "../actions/actions";
+import { useSelector } from "react-redux";
+
 import { RootStore } from "../reducers";
-import { UserState } from "../states/states";
+
 
 export const CreatePost: React.FC = () => {
-  const userString = sessionStorage.getItem("user");
-  const sessionUser = JSON.parse(userString||"");
-  const dispatch = useDispatch();
   const user = useSelector((state: RootStore) => state.login);
 
-  // useEffect(() => {
-  //   setUser(sessionUser);
-  // }, []);
-
-  const setUser = (u: UserState) => {
-    dispatch(onLogin(u));
-  };
 
   return (
     <>
