178f39c954e76f20fa14935cfc204d778cc8af8a
Robert <rob89porto@gmail.com>
2020-11-11 19:55:08 -0500

    added one enzyme test

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { shallow } from "enzyme";
 import App from "../App";
-test("renders the component", () => {
-  const component = shallow(<App />);
-  expect(component).toMatchSnapshot();
+
+describe("<App />", () => {
+  it("renders the component", () => {
+    const component = shallow(<App />);
+    expect(component).toMatchSnapshot();
+  });
 });
