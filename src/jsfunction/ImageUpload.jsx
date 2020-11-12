2212232a20fe646e3a1dadfbb1f45fcf37b509ac
TyroneV <tyroneveneracion@gmail.com>
2020-11-12 15:29:22 -0800

    updated edit profile

---
+++ src/jsfunction/ImageUpload.jsx
@@ -0,0 +1,39 @@
+import S3 from "react-aws-s3";
+import configData from "../config.json";
+
+
+const axios = require("axios");
+const url =
+  configData.SERVER_URL + "/userByEmail.app?email=sneakykey@email.com";
+export const ImageUpload = async (e) => {
+  // let updateUser = JSON.parse(sessionStorage.getItem('user'));
+  try {
+    const response = await axios.get(url);
+    const notakey = await response.data;
+
+    const config = {
+      bucketName: "mailmailmail-images",
+      dirName: "profileImages",
+      region: "us-east-2",
+      accessKeyId: notakey.firstName,
+      secretAccessKey: notakey.lastName,
+    };
+    
+    const ReactS3Client = new S3(config);
+    const newFileName = JSON.parse(sessionStorage.getItem('user')).id;
+   const data = await ReactS3Client.uploadFile(e.files[0], newFileName);
+   return data;
+  //  console.log(data);
+  //  updateUser.photo = data.key;
+
+  //  await axios.put(configData.SERVER_URL + "/updateUser.app", updateUser);
+
+  } catch (error) {
+    console.log("====================================");
+    console.log(error);
+    console.log("====================================");
+  }
+};
+
+export default ImageUpload;
+
