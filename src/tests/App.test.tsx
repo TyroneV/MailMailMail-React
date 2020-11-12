0d6b6351e4e9619fd64576b27b1fbf1f1f8e850e
Robert <rob89porto@gmail.com>
2020-11-12 10:24:09 -0500

    test works

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import { shallow } from "enzyme";
 import App from "../App";
 
-describe("<App />", () => {
-  it("renders the component", () => {
-    const component = shallow(<App />);
-    expect(component).toMatchSnapshot();
-  });
+test("renders the App component", () => {
+  const component = shallow(<App />);
+  expect(component).toMatchSnapshot();
 });
