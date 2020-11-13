58119fbcd494d3fb25a0a90a4c489ba573dead89
enochcho <enochcho97@hotmail.com>
2020-11-12 22:14:12 -0500

    trying to make my test work

--- src/tests/AutCompleteBarComponent.test.tsx
+++ src/tests/AutCompleteBarComponent.test.tsx
@@ -1,15 +1,15 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
-// import toJson from 'enzyme-to-json';
-// //import configureStore from 'redux-mock-store'; // Smart components
+import React from 'react';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+//import configureStore from 'redux-mock-store'; // Smart components
 
-// import {AutoCompleteBar} from '../components/AutoCompleteBar';
+import {AutoCompleteBar} from '../components/AutoCompleteBar';
 
-// describe ('<AutoCompleteBar />', () =>{
-//     test('renders AutoCompleteBar', () =>{
-//         const wrapper = shallow(<AutoCompleteBar/>);
-//         const component = wrapper.dive();
+describe ('<AutoCompleteBar />', () =>{
+    test('renders AutoCompleteBar', () =>{
+        const wrapper = shallow(<AutoCompleteBar/>);
+        const component = wrapper.dive();
 
-//         expect(toJson(component)).toMatchSnapshot();
-//     });
-// });
+        expect(toJson(component)).toMatchSnapshot();
+    });
+});
