b9eb2f8d3d2a38296573fe23e6a7e1bff1d83854
enochcho <enochcho97@hotmail.com>
2020-11-12 20:28:24 -0500

    not ready test

---
+++ src/tests/AutCompleteBarComponent.test.tsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+// import toJson from 'enzyme-to-json';
+// //import configureStore from 'redux-mock-store'; // Smart components
+
+// import {AutoCompleteBar} from '../components/AutoCompleteBar';
+
+// describe ('<AutoCompleteBar />', () =>{
+//     test('renders AutoCompleteBar', () =>{
+//         const wrapper = shallow(<AutoCompleteBar/>);
+//         const component = wrapper.dive();
+
+//         expect(toJson(component)).toMatchSnapshot();
+//     });
+// });
--- src/tests/FeedComponent.test.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import toJson from 'enzyme-to-json';
-import configureStore from 'redux-mock-store'; // Smart components
-
